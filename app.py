7ea8717e50b6d5c631371ed2fb5c98fd95a44f25
ANKIT RAJ <96227537+LUAMICIFER@users.noreply.github.com>
2025-03-17 15:37:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 import requests
 from bs4 import BeautifulSoup
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
 @app.route("/index")
 def index():
     my_array = ["Android", "Web_Development", "AI", "UI/UX"]
     return jsonify(my_array)
-if __name__ =="__main__":
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    # Run the app on all network interfaces (0.0.0.0) and set the port (use port 5000 or any port Render provides)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
